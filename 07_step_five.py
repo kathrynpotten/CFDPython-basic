8a19581f98bd365d57b96fc5f055f6b9dc5e9d2c
Kathryn Potten <kathrynpotten@users.noreply.github.com>
2023-08-17 15:45:14 +0100

    step 5

--- 07_step_five.py
+++ 07_step_five.py
@@ -21,7 +21,7 @@ u_init = np.ones((ny, nx))
 u_init[int(0.5 / dy) : int(1 / dy + 1), int(0.5 / dx) : int(1 / dx + 1)] = 2
 
 
-def convection2d(u, nt, c, dx, dy, dt):
+def linearconv2d(u, nt, c, dx, dy, dt):
     for _ in range(nt + 1):
         un = u.copy()
         u[1:, 1:] = (
@@ -43,7 +43,7 @@ ax = fig.add_subplot(1, 2, 1, projection="3d")
 X, Y = np.meshgrid(x, y)
 surf_init = ax.plot_surface(X, Y, u_init[:], cmap=cm.viridis)
 
-u = convection2d(u_init, nt, c, dx, dy, dt)
+u = linearconv2d(u_init, nt, c, dx, dy, dt)
 ax = fig.add_subplot(1, 2, 2, projection="3d")
 surf = ax.plot_surface(X, Y, u[:], cmap=cm.viridis)
 
